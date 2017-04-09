42732fb9df90ca78221e34b80ca64c403ce3d91f
Wes <wes.vanvugt@student.ufv.ca>
2017-04-09 16:56:58 -0700

    Added more unit tests

--- js/test/index.js
+++ js/test/index.js
@@ -14,8 +14,16 @@ function wait(time) {
 function expectTimes(a, b, message) {
     chai_1.expect(a).to.have.lengthOf(b.length, message);
     a.forEach((val, i) => {
-        chai_1.expect(val).to.be.within(b[i] * tick - tolerance, b[i] * tick + tolerance, message);
+        chai_1.expect(val).to.be.within(b[i] * tick - tolerance, b[i] * tick + tolerance, message + " (" + i + ")");
     });
+}
+function sum(nums) {
+    let total = 0;
+    let i;
+    for (i of nums) {
+        total += i;
+    }
+    return total;
 }
 describe("Concurrency Test", () => {
     it("No Limit", (done) => {
@@ -64,6 +72,63 @@ describe("Concurrency Test", () => {
             concurrencyLimit: 2,
         }).then((results) => {
             expectTimes(results, [1, 1, 2], "Timing Results");
+            done();
+        }).catch(done);
+    });
+});
+describe("Task Secializations Test", () => {
+    it("Single Task", (done) => {
+        let pool = new Pool.PromisePoolExecutor();
+        let start = Date.now();
+        let iteration = 0;
+        pool.addSingleTask({
+            data: "test",
+            generator: (data) => {
+                chai_1.expect(data).to.equal("test");
+                // The task cannot run more than once
+                chai_1.expect(iteration++).to.equal(0);
+                return wait(tick)
+                    .then(() => {
+                    return Date.now() - start;
+                });
+            }
+        }).then((result) => {
+            // The task must return the expected non-array result
+            expectTimes([result], [1], "Timing Results");
+            done();
+        }).catch(done);
+    });
+    it("Each Task", (done) => {
+        let pool = new Pool.PromisePoolExecutor();
+        let start = Date.now();
+        pool.addEachTask({
+            data: [3, 2, 1],
+            generator: (element) => {
+                return wait(tick * element)
+                    .then(() => {
+                    return Date.now() - start;
+                });
+            },
+            concurrencyLimit: Infinity,
+        }).then((results) => {
+            expectTimes(results, [3, 2, 1], "Timing Results");
+            done();
+        }).catch(done);
+    });
+    it("Batch Task", (done) => {
+        let pool = new Pool.PromisePoolExecutor();
+        let start = Date.now();
+        pool.addBatchTask({
+            data: [3, 1, 2],
+            batchSize: 2,
+            generator: (data) => {
+                return wait(tick * sum(data))
+                    .then(() => {
+                    return Date.now() - start;
+                });
+            }
+        }).then((results) => {
+            expectTimes(results, [4, 2], "Timing Results");
             done();
         }).catch(done);
     });
--- ts/test/index.ts
+++ ts/test/index.ts
@@ -15,8 +15,17 @@ function wait(time: number): Promise<void> {
 function expectTimes(a: number[], b: number[], message: string) {
     expect(a).to.have.lengthOf(b.length, message);
     a.forEach((val, i) => {
-        expect(val).to.be.within(b[i] * tick - tolerance, b[i] * tick + tolerance, message);
+        expect(val).to.be.within(b[i] * tick - tolerance, b[i] * tick + tolerance, message + " (" + i + ")");
     });
+}
+
+function sum(nums: number[]): number {
+    let total: number = 0;
+    let i: number;
+    for (i of nums) {
+        total += i;
+    }
+    return total;
 }
 
 describe("Concurrency Test", () => {
@@ -71,6 +80,69 @@ describe("Concurrency Test", () => {
             concurrencyLimit: 2,
         }).then((results) => {
             expectTimes(results, [1, 1, 2], "Timing Results");
+            done();
+        }).catch(done);
+    });
+});
+
+describe("Task Secializations Test", () => {
+    it("Single Task", (done) => {
+        let pool: Pool.PromisePoolExecutor = new Pool.PromisePoolExecutor();
+
+        let start: number = Date.now();
+        let iteration: number = 0;
+        pool.addSingleTask({
+            data: "test",
+            generator: (data) => {
+                expect(data).to.equal("test");
+                // The task cannot run more than once
+                expect(iteration++).to.equal(0);
+                return wait(tick)
+                    .then(() => {
+                        return Date.now() - start;
+                    });
+            }
+        }).then((result) => {
+            // The task must return the expected non-array result
+            expectTimes([result], [1], "Timing Results");
+            done();
+        }).catch(done);
+    });
+
+    it("Each Task", (done) => {
+        let pool: Pool.PromisePoolExecutor = new Pool.PromisePoolExecutor();
+
+        let start: number = Date.now();
+        pool.addEachTask({
+            data: [3, 2, 1],
+            generator: (element) => {
+                return wait(tick * element)
+                    .then(() => {
+                        return Date.now() - start;
+                    });
+            },
+            concurrencyLimit: Infinity,
+        }).then((results) => {
+            expectTimes(results, [3, 2, 1], "Timing Results");
+            done();
+        }).catch(done);
+    });
+
+    it("Batch Task", (done) => {
+        let pool: Pool.PromisePoolExecutor = new Pool.PromisePoolExecutor();
+
+        let start: number = Date.now();
+        pool.addBatchTask({
+            data: [3, 1, 2],
+            batchSize: 2,
+            generator: (data) => {
+                return wait(tick * sum(data))
+                    .then(() => {
+                        return Date.now() - start;
+                    });
+            }
+        }).then((results) => {
+            expectTimes(results, [4, 2], "Timing Results");
             done();
         }).catch(done);
     });
